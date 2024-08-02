d5f54be569b31f8f5724551a7fe2759267c8af7d
Micah sherry <MicahJsherry@gmail.com>
2024-08-02 14:57:39 -0400

    added pca

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import numpy as np 
 import os 
 from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
+
 import matplotlib.pyplot as plt 
 embeding_dir = "../embedings"
 
@@ -16,16 +18,17 @@ for file in os.listdir(embeding_dir):
     data = np.load(path)
     emb = data['embedding']
     if emb_list is None:
-        emb_list = emb.mean(axix=0)
+        emb_list = emb.mean(axis=0)
     else:
-        emb_list = np.vstack((emb_list, emb.mean(axix=0)))
+        emb_list = np.vstack((emb_list, emb.mean(axis=0)))
     print(path, emb.shape)
     del emb
     del data
 print(emb_list)
 
-x = emb.sum(axis=1)
-plt.scatter(x,x)
+pca = PCA(n_components=2)
+x=pca.fit_transform(emb_list)
+print(plt.scatter(x[:0],x[:1]))
 plt.savefig("scater.png")
     
 #dbscan_cluster_model = DBSCAN(eps=0.9, min_samples=3).fit(emb_list)
