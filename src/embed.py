009ecb9aa7d61acef7af7c4ffb3458eca7cc950b
Micah sherry <MicahJsherry@gmail.com>
2024-08-02 12:27:57 -0400

    created embeding script; and minor changes

---
+++ src/embed.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+import os
+
+import numpy as np
+
+import openl3
+import soundfile as sf
+
+
+DIMENSIONS = 32
+RESAMPLE_RATE = 160000
+
+data_dir = "../data"
+
+genre_dirs = os.listdir(data_dir)
+
+count = 0
+ds = {} #
+ # Output layer for a 32-dimensional fixed-length vector
+y = []
+
+emb_list = []
+
+#model = openl3.models.load_audio_embedding_model(input_repr="mel256", content_type="music",
+#                                               embedding_size=512)
+for genre in genre_dirs:
+    wav_files = os.listdir(f"{data_dir}/{genre}")
+    for file in wav_files:
+        #print(f"{data_dir}/{genre}/{file}")
+
+        try:
+            filepath = f"{data_dir}/{genre}/{file}"
+            openl3.process_audio_file(filepath, output_dir='../embedings',overwrite=True)
+        except:
+            count += 1
+
