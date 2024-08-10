1c3f370c3386df31ab95b746c6ee2f6bb52dfa65
Micah sherry <MicahJsherry@gmail.com>
2024-08-10 13:40:18 -0400

    added affinty propagation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import os 
-from sklearn.cluster import HDBSCAN, DBSCAN, KMeans
+from sklearn.cluster import HDBSCAN, DBSCAN, KMeans, AffinityPropagation
 from sklearn.decomposition import PCA
 
 from sklearn.manifold import TSNE 
@@ -8,7 +8,6 @@ from sklearn.manifold import TSNE
 from sklearn.preprocessing import LabelEncoder
 
 import matplotlib.pyplot as plt 
-from sklearn.metrics import  silhouette_score
 
 from recomend import *
     
@@ -47,47 +46,46 @@ x=pca.fit_transform(emb_list)
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(y_list)
 
-ss_genre = silhouette_score(emb_list, y_encoded)    
 #print(y_encoded)
 
 kmeans = KMeans(n_clusters=10, random_state=0, n_init="auto").fit(emb_list)
 y_kmeans = kmeans.labels_
-ss_kmeans = silhouette_score(emb_list, y_kmeans)    
 
 hdbscan = HDBSCAN(min_cluster_size=10, min_samples=5).fit(emb_list)
 y_hdbscan = hdbscan.labels_
-ss_hdbscan = silhouette_score(emb_list, y_hdbscan) 
 
-dbscan= DBSCAN(eps=12.2, min_samples=5).fit(emb_list)
+dbscan  = DBSCAN(eps=4.2, min_samples=5).fit(emb_list)
 y_dbscan = dbscan.labels_
-ss_dbscan = silhouette_score(emb_list, y_dbscan) 
 
+affinity = AffinityPropagation(eps=12.2, min_samples=5).fit(emb_list)
+y_affinity = affinity.labels_
 
 
 plt.scatter(x[:,0],x[:,1],c=y_encoded, cmap='viridis')
 plt.title("music grouped by Genre")
-plt.text(20,10, f"silhouette_score:{ss_genre:.2f}")
 plt.savefig(f"{figs}/scater_true.png")
 plt.clf()
 
 plt.scatter(x[:,0],x[:,1], c=y_kmeans, cmap='viridis')
 plt.title("music grouped by K-Means clustering")
-plt.text(20,10, f"silhouette_score:{ss_kmeans:.2f}")
 plt.savefig(f"{figs}/scater_kmeans.png")
 plt.clf()
 
 
 plt.scatter(x[:,0],x[:,1], c=y_hdbscan, cmap='viridis')
 plt.title(f"music grouped by HDBSCAN clustering")
-plt.text(20,10, f"silhouette_score:{ss_hdbscan:.2f}")
 plt.savefig(f"{figs}/scater_HDBSCAN.png")
 plt.clf()
 
 
 plt.scatter(x[:,0],x[:,1], c=y_dbscan, cmap='viridis')
 plt.title("music grouped by DBSCAN clustering")
-plt.text(20,10, f"silhouette_score:{ss_dbscan:.2f}")
 plt.savefig(f"{figs}/scater_DBSCAN.png")
+plt.clf()
+
+plt.scatter(x[:,0],x[:,1], c=y_affinity, cmap='viridis')
+plt.title("music grouped by Affinity Propagation clustering")
+plt.savefig(f"{figs}/scater_AffinityPropagation.png")
 plt.clf()
 
 
